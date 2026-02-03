b43c495aedea23d3e75d29f5d30ef3ff17553f74
Patrick Corrêa <patrickcs352@gmail.com>
2026-02-03 20:16:44 -0300

    commit-new-useState

--- To-Do-App/todo-app/src/App.jsx
+++ To-Do-App/todo-app/src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from "react"
 
 function App() {
 
+  // UseState
   const [task, setTask ] = useState("");
+
+  const [text, setText] = useState("");
 
   return (
     <div>
       <h1>Lista de Tarefas: </h1>
-      <input type="text" />
+      <input type="text" value={text} onChange={(e) => setText(e.target.value)}/>
       <button>Adicionar: </button>
     </div>
   )
