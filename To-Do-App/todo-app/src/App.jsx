c6e43c48d72b15a6d4716e11e0369bc9ece86ee4
Patrick Corrêa <patrickcs352@gmail.com>
2026-02-05 19:44:14 -0300

    commit-correção(função-adicionar)

--- To-Do-App/todo-app/src/App.jsx
+++ To-Do-App/todo-app/src/App.jsx
@@ -7,10 +7,26 @@ function App() {
   // Texto do input
   const [text, setText] = useState("");
 
+  // Função de adicionar
+  const addTask = () => {
+    if (text.trim() === "") {
+      return;
+
+      const newTask = {
+        id: Date.now, // id único para cada tarefa
+        texto: text, // texto da tarefa
+        concluida: false, // status da tarefa
+      };
+
+      setTask((previousTask) => [...previousTask, newTask]);
+      setText("");
+    }
+  }
+
   return (
     <div>
       <h1>Lista de tarefas: </h1>
-      <input type="text" value={text} onChange={(e) => setTask(e.target.value)}/>
+      <input type="text" value={text} onChange={(e) => setTask(e.target.value)} />
       <button>Adicionar</button>
     </div>
   );
