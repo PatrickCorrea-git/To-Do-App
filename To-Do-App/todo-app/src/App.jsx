3ddcffb12fc8f5df697cbae2004e0a90395235a9
Patrick Corrêa <patrickcs352@gmail.com>
2026-02-05 19:22:30 -0300

    commit-correção(estados)

--- To-Do-App/todo-app/src/App.jsx
+++ To-Do-App/todo-app/src/App.jsx
@@ -1,38 +1,11 @@
-import { useState } from "react"
+import { useState } from "react";
 
 function App() {
-
-  // Estado para as tarefas
-  const [task, setTask] = useState([]);
-
-  // Estado para o texto
-  const [text, setText] = useState("");
-
-  // Função chamada ao clicar no botão
-  const addTask = () => {
-    if (text.trim() === "")
-      return;
-
-    const newTask = {
-      id: Date.now(), // id único para cada tarefa
-      texto: text, // texto da tarefa
-      concluida: false, // status
-    };
-
-    setTask((previousTasks) => [...previousTasks, newTask]);
-    setText("");
-  }
+  const [task, setTask]
 
   return (
     <div>
-      <h1>Lista de Tarefas: </h1>
-      <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
-      <button onClick={addTask}>Adicionar: </button>
-      <ul>
-        {task.map((taskItem) => (
-          <li key={taskItem.id}>{taskItem.texto}</li>
-        ))}
-      </ul>
+      
     </div>
   );
 }
