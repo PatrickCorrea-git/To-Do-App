b3fa89cc95922356a4c4e93a3fd3f905de727b5c
Patrick Corrêa <patrickcs352@gmail.com>
2026-02-05 18:00:21 -0300

    Update App.jsx

--- To-Do-App/todo-app/src/App.jsx
+++ To-Do-App/todo-app/src/App.jsx
@@ -2,18 +2,38 @@ import { useState } from "react"
 
 function App() {
 
-  // UseState
-  const [task, setTask ] = useState("");
+  // Estado para as tarefas
+  const [task, setTask] = useState([]);
 
+  // Estado para o texto
   const [text, setText] = useState("");
+
+  // Função chamada ao clicar no botão
+  const addTask = () => {
+    if (text.trim() === "")
+      return;
+
+    const newTask = {
+      id: Date.now(), // id único para cada tarefa
+      texto: text, // texto da tarefa
+      concluida: false, // status
+    };
+
+    setTask((previousTasks) => [...previousTasks, newTask]);
+    setText("");
+  }
 
   return (
     <div>
       <h1>Lista de Tarefas: </h1>
-      <input type="text" value={text} onChange={(e) => setText(e.target.value)}/>
-      <button>Adicionar: </button>
+      <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
+      <button onClick={addTask}>Adicionar: </button>
+      <ul>
+        {task.map((taskItem) => (
+          <li key={taskItem.id}>{taskItem.texto}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
-
 export default App
