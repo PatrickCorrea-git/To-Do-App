7da7079f8284026505a93b70fc2e7e64f1111034
Patrick Corrêa <patrickcs352@gmail.com>
2026-02-05 19:24:14 -0300

    Update App.jsx

--- To-Do-App/todo-app/src/App.jsx
+++ To-Do-App/todo-app/src/App.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
 
 function App() {
-  const [task, setTask]
+  // Estado para lista de Tarefas
+  const [task, setTask] = useState([]);
+
+  // Texto do input
+  const [text, setText] = useState("");
 
   return (
     <div>
